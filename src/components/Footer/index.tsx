8a95d436910f277601181969e232a0b945aa8df6
BerryBie <794072950@qq.com>
2023-04-04 20:39:06 +0800

    First upload.

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,33 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import {PLANET_LINK} from "@/constants";
+const Footer: React.FC = () => {
+  const defaultMessage = 'BerryBie';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'Ant Design Pro',
+          title: '知识星球',
+          href: PLANET_LINK,
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <><GithubOutlined />Berry的GitHub</>,
+          href: 'https://github.com/ant-design/ant-design-pro',
+          blankTarget: true,
+        },
+        {
+          key: 'Ant Design',
+          title: '编程导航',
+          href: 'https://www.code-nav.cn',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+export default Footer;
---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export const SYSTEM_LOGO = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/001_Chateau_de_Chillon_and_Dents_du_Midi_Photo_by_Giles_Laurent.jpg/1350px-001_Chateau_de_Chillon_and_Dents_du_Midi_Photo_by_Giles_Laurent.jpg";
+
+/**
+ * 菜鸟地址
+ */
+
+export const PLANET_LINK = "https://www.runoob.com/java/java-tutorial.html";
